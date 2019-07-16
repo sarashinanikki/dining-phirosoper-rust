85f6bc8ce27bbf501e7fd13603907ad9077ef236
sarashin <sarashina.nikki65@gmail.com>
2019-07-16 14:07:30 +0900

    make skelton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+struct Philosoper {
+    name: String,
+}
+
+impl Philosoper {
+    fn new(name: &str) -> Philosoper {
+        Philosoper {
+            name: name.to_string(),
+        }
+    }
+
+    fn eat(&self) {
+        println!("{} is done eating!", self.name);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let Philosopers = vec![
+        Philosoper::new("Socrates");
+        Philosoper::new("Plato");
+        Philosoper::new("Aristoteles");
+        Philosoper::new("Anaximandros");
+        Philosoper::new("Epikouros");
+    ];
+
+    for p in Philosopers {
+        p.eat();
+    }
 }
